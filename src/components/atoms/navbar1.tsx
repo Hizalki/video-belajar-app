52e0e926a6f37e09fab8bfec46e10270cf70e97f
Hizalki <hizalkiarsi@gmail.com>
2025-06-16 00:31:49 +0700

    tambahan

--- src/components/atoms/navbar1.tsx
+++ src/components/atoms/navbar1.tsx
@@ -6,15 +6,15 @@ function Navbar() {
 
     return(
     <div>
-        <header className='bg-white'>
-            <nav className="pb-20 ...">
-                <div className="flex items-center justify-between" >
+        <header className='bg-white border-b border-b-[rgba(58,53,65,0.12)] shadow-xl '>
+            <nav className="flex gap-9 py-3 px-[120px] items-center " >
+                <div className="flex justify-between flex-1" >
                     <a href="#">
                     <img src={logo} alt="logo" />
                 </a>
                 <p>Kategori</p>
-                </div>
-                <div>
+                </div >
+                <div className='inline'>
                     <img src={avatar} alt="Profil" />
                 </div>
             </nav>
